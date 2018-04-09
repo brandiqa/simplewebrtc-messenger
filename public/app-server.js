f4adabbde494db79a0857424523001bdc87722e9
Michael Wanyoike <micwanyoike@gmail.com>
2018-04-10 02:33:53 +0300

    Implemented https

--- public/app-server.js
+++ public/app-server.js
@@ -1,5 +1,20 @@
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 const express = require('express');
+
+const port = 3000;
+
+var options = {
+  key: fs.readFileSync('./ssl/server.key'),
+  cert: fs.readFileSync('./ssl/server.crt'),
+};
+
 const app = express();
 
+const server = https.createServer(options, app).listen(port, function () {
+  console.log("Express server listening on port " + port);
+});
+
 app.use(express.static('./'));
-app.listen(3000);
+
