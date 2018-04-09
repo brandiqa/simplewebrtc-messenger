f1f017da2cd21d8bd20a7af1b70ff1ac7a16fb97
Michael Wanyoike <micwanyoike@gmail.com>
2018-04-09 23:31:27 +0300

    Configured express to serve index,html

--- public/app-server.js
+++ public/app-server.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-});
-
+app.use(express.static('./'));
 app.listen(3000);
