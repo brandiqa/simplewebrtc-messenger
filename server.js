a779b8d44c82b5349fc4c2d05053dc8f7c0723bf
Michael Wanyoike <micwanyoike@gmail.com>
2018-04-24 13:50:10 +0300

    Re-organized project structure

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+// Set public folder as root
+app.use(express.static('public'));
+
+// Provide access to node_modules folder
+app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+
+// Redirect all traffic to index.html
+app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.info('listening on %d', port);
+});
+
+
+
