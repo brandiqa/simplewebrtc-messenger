cd9dbea7948243ef8300add089f0699cf5e2670e
Michael Wanyoike <micwanyoike@gmail.com>
2018-04-24 14:40:42 +0300

    Implemented basic UI with loacal video

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,23 @@
 window.addEventListener('load', () => {
 
+  const localVideo = $('#my-camera video')[0];
+
+  const onReceiveStream = (stream, video) => {
+    video.srcObject = stream;
+    window.peer_stream = stream;
+  }
+
+  const getVideo = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+      window.localStream = stream;
+      onReceiveStream(stream, localVideo);
+    } catch (error) {
+      console.log(error);
+      alert('An error occurred. Please try again');
+    }
+  }
+
+  getVideo();
+
 })
