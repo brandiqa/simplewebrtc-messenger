4b7d504b5df793a2ee8369feea5667009423da8a
Michael Wanyoike <micwanyoike@gmail.com>
2018-04-24 19:54:25 +0300

    Added Create Room Form

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,29 @@
 window.addEventListener('load', () => {
 
-  const localVideo = $('#my-camera video')[0];
+  const roomChatTemplate = Handlebars.compile($('#chat-room-template').html());
+
+  const myCamera = $('#my-camera');
+  const peerCamera = $('#peer-camera');
+  const myImage = $('#my-image');
+  const peerImage = $('#peer-image');
+  const room = $('#room-wrapper');
+
+  // Hide cameras until they are initialized
+  myCamera.hide();
+  peerCamera.hide();
+
+  // Create Room Form Validation Rules
+  $('form').form({
+    fields: {
+      room: 'empty',
+    },
+  });
+  $('.submit').on('click', () => {
+    const roomName = $('#room').val();
+    console.log(roomName);
+    $('form').form('clear');
+    return false;
+  });
 
   const onReceiveStream = (stream, video) => {
     video.srcObject = stream;
@@ -11,13 +34,16 @@ window.addEventListener('load', () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
       window.localStream = stream;
-      onReceiveStream(stream, localVideo);
+      const video = myCamera.find('video')[0];
+      onReceiveStream(stream, video);
+      myImage.hide();
+      myCamera.show();
     } catch (error) {
       console.log(error);
       alert('An error occurred. Please try again');
     }
   }
 
-  // getVideo();
+  getVideo();
 
 })
